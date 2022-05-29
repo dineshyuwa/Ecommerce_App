8ee7a5bc90f9e0c404c371da074c9635c203f404
Yuwarajalingam Dinesh <yuwarajalingamd@gmail.com>
2022-05-29 12:58:06 +0530

    added product and banner schema

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <>
+     Herobanner
+     <div>
+       <h2>Best delling products</h2>
+       <p>Speakers of many variations</p>
+     </div>
+     <div>
+       ['Product 1','Product 2']
+     </div>
     </>
   )
 }
